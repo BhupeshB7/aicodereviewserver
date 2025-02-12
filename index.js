50be13f6b6fe0083de6b7663b49764a934ae5953
BhupeshB7 <bhupeshkr2912@gmail.com>
2025-02-13 03:24:51 +0530

    cors

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.get("/", (req, res) => {
 });
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["https://aicodereviewb7.netlify.app", "http://localhost:5173"],
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     allowedHeaders: "Content-Type, Authorization",
